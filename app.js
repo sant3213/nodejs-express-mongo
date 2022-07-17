31c24586e484061b234ad1f2bc87cadc718d68b7
Sant <ssnt@Sants-Mac-mini.local>
2022-07-16 19:04:41 -0500

    feat: Add routing for sessions

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-
+const sessionsRouter = express.Router()
 app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -14,6 +14,16 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');/* allow us to set variables inside the context of our app*/
 app.set('view engine', 'ejs');
 
+sessionsRouter.route('/').get((req, res)=> {
+    res.render('sessions', { sessions: [
+        {title: 'Session 1', description:'This is session 1'},
+        {title: 'Session 2', description:'This is session 2'},
+        {title: 'Session 3', description:'This is session 4'},
+        {title: 'Session 4', description:'This is session 4'}
+    ]})
+});
+
+app.use('/sessions', sessionsRouter);
 
 app.get('/',(req, res)=>{
     res.render('index', {title: ' Surymantics', data: ['First','Second','Third']})
